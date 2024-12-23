92489a2fce061b943d17230d5ea29a1e9ca8930b
Lahoussine Amhal <lamhal@e3r3p5.1337.ma>
2024-12-23 09:50:53 +0100

    wall colesion bonus mandatory

--- Cub3d/mandatory/raycast/moves.c
+++ Cub3d/mandatory/raycast/moves.c
@@ -6,7 +6,7 @@
 /*   By: lamhal <lamhal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/22 09:21:56 by lamhal            #+#    #+#             */
-/*   Updated: 2024/12/22 10:02:03 by lamhal           ###   ########.fr       */
+/*   Updated: 2024/12/23 09:40:24 by lamhal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,44 +21,50 @@ int	check_map(t_data *data, double x, double y)
 	j = y / TILE_SIZE;
 	if (data->map.map[j][i] == '0')
 		return (1);
-    // i = (x + 10) / TILE_SIZE;
-	// j = (y + 10) / TILE_SIZE;
-	// if (data->map.map[j][i] == '0')
-	// 	return (1);
 	return (0);
 }
 
 void	move_front_back(t_data *data, int dirc)
 {
 	t_pos	pos;
+	t_pos	check;
+	int		dst_to_wall;
 
+	dst_to_wall = MOV_SPEED + 20;
 	pos.x = data->player.x + dirc * MOV_SPEED * cos(data->ang);
 	pos.y = data->player.y + dirc * MOV_SPEED * sin(data->ang);
-	if (check_map(data, pos.x, pos.y))
+	check.x = data->player.x + dirc * dst_to_wall * cos(data->ang);
+	check.y = data->player.y + dirc * dst_to_wall * sin(data->ang);
+	if (check_map(data, pos.x, pos.y) && check_map(data, check.x, check.y))
 	{
 		data->player.x = pos.x;
 		data->player.y = pos.y;
 	}
-    else if (check_map(data, data->player.x, pos.y))
+    else if (check_map(data, data->player.x, pos.y) && check_map(data, data->player.x, check.y))
         data->player.y = pos.y;
-    else if (check_map(data, pos.x, data->player.y))
+    else if (check_map(data, pos.x, data->player.y) && check_map(data, check.x, data->player.y))
         data->player.x = pos.x;
 }
 
 void	move_right_left(t_data *data, int dirc)
 {
 	t_pos	pos;
+	t_pos	check;
+	int		dst_to_wall;
 
+	dst_to_wall = MOV_SPEED + 20;
 	pos.x = data->player.x + MOV_SPEED * cos(data->ang + dirc * M_PI / 2);
 	pos.y = data->player.y + MOV_SPEED * sin(data->ang + dirc * M_PI / 2);
-	if (check_map(data, pos.x, pos.y))
+	check.y = data->player.y + dst_to_wall * sin(data->ang + dirc * M_PI / 2);
+	check.x = data->player.x + dst_to_wall * cos(data->ang + dirc * M_PI / 2);
+	if (check_map(data, pos.x, pos.y) && check_map(data, check.x, check.y))
 	{
 		data->player.x = pos.x;
 		data->player.y = pos.y;
 	}
-    else if (check_map(data, data->player.x, pos.y))
+    else if (check_map(data, data->player.x, pos.y) && check_map(data, data->player.x, check.y))
         data->player.y = pos.y;
-    else if (check_map(data, pos.x, data->player.y))
+    else if (check_map(data, pos.x, data->player.y) && check_map(data, check.x, data->player.y))
         data->player.x = pos.x;
 }
 
